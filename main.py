a1cb3a11fa97b48ab6a73be634af2b4c3b6da375
Kornelyzas <kornelgaiz@gmail.com>
2021-03-12 09:10:26 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+
+#6
+kintamasis = 'IlgasSakinys'
+x = kintamasis[2:5]
+print(x) #gas
+x = kintamasis[7:9]
+print(x) #ki
+
+
+#7
+#mutable gali keistis, immutable - ne.
+listas = ['hello', 'apple', 'one'] #mutable
+tuplas = ('hello', 'apple', 'one') #immutable
+
+#8
+
+
+
+def the_function(list):
+    print(list)
+
+
+list = [1, 2, 3]
+the_function(list)
